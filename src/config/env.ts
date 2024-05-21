763c4f251e48a5da24b91f708605e05a98d677fd
jesusNut <jesusNut@users.noreply.github.com>
2024-05-21 16:49:25 +0000

    deploy: 64397cd974570c394b3b583c629c67ef50c68540

--- src/config/env.ts
+++
@@ -1,7 +0,0 @@
-import { decrypt } from "../utils/CryptojsUtil";
-
-export class env {
-  public static readonly BASE_URL = decrypt(process.env.URL!);
-  public static readonly USERID = decrypt(process.env.USERID!);
-  public static readonly PASSWORD = decrypt(process.env.PASSWORD!);
-}
