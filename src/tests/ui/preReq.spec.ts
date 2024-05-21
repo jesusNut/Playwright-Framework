f7f0d29c3e2b547fd35e18f08c2d11491fde3f1b
jesusNut <bhardwajabhishek.qa@gmail.com>
2024-05-21 15:47:04 +0530

    Commit : 21 : Modifications in github actions workflow for debugging

--- src/tests/ui/preReq.spec.ts
+++ src/tests/ui/preReq.spec.ts
@@ -7,23 +7,23 @@
 // import { generateTestData, exportToCsv, exportToJson } from "@utils/FakerDataUtil";
 
 // test("sample test to know environment variables health", () => {
-//   console.log(process.env.NODE_ENV);
-//   console.log(process.env.USER);
-//   console.log(process.env.PASSWORD);
-//   console.log(decrypt(env.USERNAME));
-//   console.log(decrypt(env.PASSWORD));
-//   console.log(decrypt(env.BASE_URL));
+// //   console.log(process.env.NODE_ENV);
+// //   console.log(process.env.USER);
+// //   console.log(process.env.PASSWORD);
+// //   console.log(decrypt(env.USERNAME));
+// //   console.log(decrypt(env.PASSWORD));
+// //   console.log(decrypt(env.BASE_URL));
 //   console.log("************************");
 //   //   console.log(env.BASE_URL);
 //   //   console.log(env.USERNAME);
 //   //   console.log(env.PASSWORD);
 
-//   //   const plaintext = "Hello, Mars!";
+//      const plaintext = "U2FsdGVkX1++hKWNhbBd/uA31z4i/xGRIqUfyK7UTICj3ssn3Q5O3SGQilaAcug64hjj2KsVTz3BG0GHtbVHYQ==";
 //   //   console.log(`Salt is : ${process.env.SALT}`);
 //   //   const encryptedText = encrypt(plaintext);
 //   //   console.log(`Encrypted text is ${encryptedText}`);
-//   //   const decryptedtext = decrypt(encryptedText);
-//   //   console.log(`Decrypted text is ${decryptedtext}`);
+//      const decryptedtext = decrypt(plaintext);
+//      console.log(`Decrypted text is ${decryptedtext}`);
 // });
 
 // test("Run me to encrypt the env files", () => {
